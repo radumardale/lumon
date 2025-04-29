b8079ac019871f7f35fc116293f86b8fcec94d18
Radu Mardale <radu.mardale@gmail.com>
2025-04-29 20:03:23 +0300

    Jest working

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '\\.(css)$': 'identity-obj-proxy',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
-    '^@styles/(.*)$': '<rootDir>/src/styles/$1'
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
   },
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.stories.tsx'],
   coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov']
+  coverageReporters: ['text', 'lcov'],
 };
